fd836b36e515d357b8084803de6de71302db57ef
Josh Spicer <josh@joshspicer.com>
2022-09-14 15:18:39 +0000

    simple server and communication between

--- client/js/app/five-in-a-row.js
+++ client/js/app/five-in-a-row.js
@@ -4,6 +4,17 @@ var rows = 19;
 var columns = 19;
 var size = 50;
 
+const EMPTY = 0;
+const BLACK = 1;
+const WHITE = 2;
+
+const initial_state = {
+  board: new Array(rows).fill(new Array(columns).fill(EMPTY)),
+  turn: BLACK,
+}
+
+var state;
+
 function mapClickToGrid(x, y) {
   xx = Math.floor(x / size);
   yy = Math.floor(y / size);
@@ -11,8 +22,33 @@ function mapClickToGrid(x, y) {
   return {xIdx: xx,  yIdx: yy }
 }
 
-function fillRect(tile, color) {
-  ctx.fillStyle = "black";
+// POSTs move to server
+function tryCommitMove(tile) {
+
+  // POST to server
+  const url = "http://localhost:5000/checkMove";
+  const data = {tile: tile, turn: state.turn};
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+  fetch(url, options)
+    .then(res => {
+      res.json().then(data => {
+        console.log(data);
+      });
+
+      // Update the global state.
+      // console.log(state);
+      return true;
+    })
+}
+
+function displayMove(tile) {
+  ctx.fillStyle = state.turn === BLACK ? "black" : "white";
   ctx.fillRect(tile['xIdx'] * size, tile['yIdx'] * size, size, size);
 }
 
@@ -20,11 +56,14 @@ function loadGame() {
   canvas = document.getElementById("gameCanvas");
   ctx = canvas.getContext("2d");
   displayGrid(ctx);
+  state = initial_state;
+  console.log(state);
   
-  canvas.onmousedown = function(event){
+  canvas.onmousedown = function(event) {
     console.log("x: " + event.offsetX + " y: " + event.offsetY);
     const tile = mapClickToGrid(event.offsetX - 10, event.offsetY - 10);
-    fillRect(tile, "red");
+    
+    tryCommitMove(tile);
   };
 }
 
