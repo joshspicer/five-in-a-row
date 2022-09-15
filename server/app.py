ebdfeb2b95a21b8f0536281ba69ac8b65c051866
Josh Spicer <josh@joshspicer.com>
2022-09-15 02:20:11 +0000

    before web sockets

--- server/app.py
+++ server/app.py
@@ -1,15 +1,19 @@
 from flask import Flask
+from flask import request
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/registerPlayer', methods=['POST'])
-def registerPlayer():
-    # TODO
-    return 'YOU ARE PLAYER X'
+# @app.route('/registerPlayer', methods=['POST'])
+# def register_player():
+#     # TODO
+#     return 'YOU ARE PLAYER X'
 
-@app.route("/checkMove", methods=["POST"])
-def checkMove():
-    return {"status": "ok"}
+
+# @app.route("/checkMove", methods=["POST"])
+# def check_move():
+#     j = request.get_json()
+#     client_board = j['board']
+#     client_turn = j['turn']
+#     return j
